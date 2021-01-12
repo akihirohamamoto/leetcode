c637b3ca1059ab1fb707d4ccafe2dfbaaf09bb6a
akihirohamamoto <giudecca1021@gmail.com>
2021-01-12 05:07:47 +0000

    [add] #2 solving

---
+++ .leetcode/2.add-two-numbers.py
@@ -0,0 +1,26 @@
+# @before-stub-for-debug-begin
+from python3problem2 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=2 lang=python3
+#
+# [2] Add Two Numbers
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+# @lc code=end
+
+# Test cases
+# [2,4,3], [5,6,4]
+# [0], [0]
+# [9,9,9,9,9,9,9], [9,9,9,9]
